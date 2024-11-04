173ec03229c0d6f875ef33352a9a15a0c197c7eb
chris-cez <christopher.310704@alunos.utfpr.edu.br>
2024-11-04 17:41:24 -0300

    first commit

---
+++ BD/AA_produtos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE produto (
+	id          serial      primary key,
+	descricao   varchar     not null    default '',
+	preco       real        not null    default 0.0,
+	estoque     int         not null    default 0,
+	data_		timestamp   not null    default now()
+);
