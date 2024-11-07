1d5ad049ce54718c5b6ca83bc4f9d212705715aa
chris-cez <christopher.310704@alunos.utfpr.edu.br>
2024-11-06 21:36:06 -0300

    first commit

--- BD/AA_produtos.sql
+++ BD/AA_produtos.sql
@@ -1,7 +1,9 @@
+\c db;
+
 CREATE TABLE produto (
 	id          serial      primary key,
 	descricao   varchar     not null    default '',
 	preco       real        not null    default 0.0,
 	estoque     int         not null    default 0,
-	data_		timestamp   not null    default now()
+	data_		timestamp   default now()
 );
---
+++ BD/AB_populate_produtos.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO produto (descricao, preco, estoque) VALUES
+('Waffer', 5.00, 50),
+('Bolacha', 3.00, 100),
+('Babaloo', 0.50, 80);
